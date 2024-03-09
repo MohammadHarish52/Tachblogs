7ed1825c38c87007531d51d04ad44ad83bc8109a
tachyonok <tachyonparticle123@gmail.com>
2024-03-09 15:13:41 +0530

    Basic signin signup done

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,8 +1,17 @@
-import { Button, Card, TextField, Typography, styled } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  TextField,
+  Typography,
+  styled,
+} from "@mui/material";
+import { useState } from "react";
 
 const LoginCard = styled(Card)`
-  width: 400px;
+  width: 350px;
   margin: auto;
+  margin-top: 5%;
   display: flex;
   flex-direction: column;
   padding: 12px;
@@ -10,25 +19,76 @@ const LoginCard = styled(Card)`
 `;
 
 const Login = () => {
+  const [login, setIsLogin] = useState(true); // true is login, false is sign
+
+  const handleCreate = () => {
+    setIsLogin((e) => !e);
+  };
   return (
     <LoginCard>
       <Typography variant="h5" color="primary">
+        {" "}
         TachBlogs
       </Typography>
-      <TextField
-        id="outlined-basic"
-        label="Username"
-        variant="outlined"
-        sx={{ marginTop: "12px" }}
-      />
-      <TextField id="outlined-basic" label="Password" variant="outlined" />
-      <Button variant="contained" sx={{ marginTop: "12px" }}>
-        Login
-      </Button>
-      <Typography variant="h7" sx={{ margin: "auto", color: "gray" }}>
-        OR
-      </Typography>
-      <Button variant="outlined">Create an Account</Button>
+
+      {login ? (
+        <Box
+          variant="outlined"
+          sx={{ display: "flex", flexDirection: "column", gap: "12px" }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+            sx={{ marginTop: "12px" }}
+          />
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+          <Button variant="contained" sx={{ marginTop: "12px" }}>
+            Login
+          </Button>
+          <Typography variant="h7" sx={{ margin: "auto", color: "gray" }}>
+            OR
+          </Typography>
+          <Button variant="outlined" onClick={handleCreate}>
+            Create an Account
+          </Button>
+        </Box>
+      ) : (
+        <Box
+          variant="outlined"
+          sx={{ display: "flex", flexDirection: "column", gap: "12px" }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Enter Name"
+            variant="outlined"
+            sx={{ marginTop: "12px" }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Enter Username"
+            variant="outlined"
+          />
+          <TextField
+            id="outlined-basic"
+            label=" Enter Password"
+            variant="outlined"
+          />
+          <Button
+            variant="contained"
+            sx={{ marginTop: "12px" }}
+            onClick={handleCreate}
+          >
+            Sign Up
+          </Button>
+          <Typography variant="h7" sx={{ margin: "auto", color: "gray" }}>
+            OR
+          </Typography>
+          <Button variant="outlined" onClick={handleCreate}>
+            Already Have an Account
+          </Button>
+        </Box>
+      )}
     </LoginCard>
   );
 };
